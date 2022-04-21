4ed7f8ea250576f033d8dfc077400b3cee9e9cef
unknown <gabrielsoares1602@gmail.com>
2022-04-21 15:48:26 -0300

    att3

--- script.js
+++ script.js
@@ -39,6 +39,9 @@ const songName = document.querySelector(".song-name")
 const artistName = document.querySelector(".artist-name")
 const coverImg = document.querySelector(".cover-img")
 
+const btnMutedOff = document.querySelector(".off")
+const btnMutedOn = document.querySelector(".up")
+
 const btnPlay = document.querySelector("#play")
 const btnPause = document.querySelector("#pause")
 const btnPrev = document.querySelector("#prev")
@@ -46,6 +49,31 @@ const btnNext = document.querySelector("#next")
 const progressBar = document.querySelector("progress")
 const pastTime = document.querySelector(".start")
 const entTime = document.querySelector(".end")
+
+const imgCover1 = document.querySelector(".next-cover-1")
+const songName1 = document.querySelector(".next-song-1")
+const artistName1 = document.querySelector(".next-artist-1")
+const imgCover2 = document.querySelector(".next-cover-2")
+const songName2 = document.querySelector(".next-song-2")
+const artistName2 = document.querySelector(".next-artist-2")
+const imgCover3 = document.querySelector(".next-cover-3")
+const songName3 = document.querySelector(".next-song-3")
+const artistName3 = document.querySelector(".next-artist-3")
+const imgCover4 = document.querySelector(".next-cover-4")
+const songName4 = document.querySelector(".next-song-4")
+const artistName4 = document.querySelector(".next-artist-4")
+
+btnMutedOn.addEventListener("click", () => {
+    song.muted = true;
+    btnMutedOff.style.display = "block"
+    btnMutedOn.style.display = "none"
+})
+
+btnMutedOff.addEventListener("click", () => {
+    song.muted = false;
+    btnMutedOn.style.display = "block"
+    btnMutedOff.style.display = "none"
+})
 
 btnPlay.addEventListener("click", () => {
     song.play()
@@ -107,13 +135,30 @@ function renderSong(index) {
     song.setAttribute("src", songs[index].src)
     song.addEventListener("loadeddata", () => {
         coverImg.src = songs[index].img
-        artistName.textContent = songs[index].artist
         songName.textContent = songs[index].title
+        artistName.textContent = songs[index].artist
         entTime.textContent = secToMin(song.duration)
         progressBar.style.width = "0%"
         btnPlay.style.display = "block"
         btnPause.style.display = "none"
+        renderNextSongs(indexSong)
     })
 }
 
+function renderNextSongs(index){
+    imgCover1.src = songs[index + 1].img
+    songName1.textContent = songs[index + 1].title
+    artistName1.textContent = songs[index + 1].artist
+    imgCover2.src = songs[index + 2].img
+    songName2.textContent = songs[index + 2].title
+    artistName2.textContent = songs[index + 2].artist
+    imgCover3.src = songs[index + 3].img
+    songName3.textContent = songs[index + 3].title
+    artistName3.textContent = songs[index + 3].artist
+    imgCover4.src = songs[index + 4].img
+    songName4.textContent = songs[index + 4].title
+    artistName4.textContent = songs[index + 4].artist
+}
+
 renderSong(indexSong)
+renderNextSongs(indexSong)
